050d8a21de3775d08ab094a9625bcf9b56afc90c
muriukikelvin <43674768+muriukikelvin@users.noreply.github.com>
2019-05-01 15:59:06 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,202 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Zaraa Farm Produce Marketers | Home :: Kendu Designers</title>
+<meta name="description" content="Zaraa Farm Produce Marketers provide farmers with a platfrom to market their produce. Farmers determine their prices. Access to agreicultural information is offered.">
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="keywords" content="Farming, Marketing , Marketer ; charset=utf-8" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<!-- Custom Theme files -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link rel="stylesheet" href="css/lightbox.css">
+<link rel="stylesheet" type="text/css" href="css/menu_topexpand.css" />
+<!-- //Custom Theme files -->
+<!--fonts-->
+<link href="//fonts.googleapis.com/css?family=Courgette&amp;subset=latin-ext" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext,vietnamese" rel="stylesheet">
+<!--//fonts-->
+</head>
+<body>
+
+<!--Slider-->
+ <nav class="show-menu">
+<div class="slider">
+	<div class="header-bottom">
+	<div class="top-nav menu-wrap">
+	    
+		<h1><a href="index.php" class="logo img-responsive"><img id="logo" src="images/logo.png"/></a></h1>
+		 <div class="icon-list">
+			<a href="#home" class="scroll"><i class="glyphicon glyphicon-home"></i><span>Home</span></a>
+			<a href="marketplace.php"><i class="glyphicon glyphicon-euro"></i><span>MarketPlace</span></a> 
+			<a href="cropblog.php"><i class="glyphicon glyphicon-info-sign"></i><span>Crop Information</span></a> 
+		    <!--<a href="#contact" class="scroll"><i class="glyphicon glyphicon-envelope"></i><span>Contact</span></a>-->
+		</div>
+	</div>
+	</div>
+	<!--<div class="callbacks_container">-->
+		<ul class="rslides" id="slider3">
+			<li>
+				<div class="slider-info">
+				<h3>Welcome to Zaraa Farm Produce Marketers</h3>
+					<p>Have a Look at our Beautiful products</p>
+					<!--<div class="readmore-w3"><a class="readmore" href="blog.php" data-toggle="modal" data-target="#myModal2">Read More</a></div>-->
+				</div>
+			</li>
+			<li>
+				<div class="slider-info">
+					<h3>Welcome to Zaraa Farm Produce Marketers</h3>
+					<p>We provide professional advice on best farming practices </p>
+					<!--<div class="readmore-w3"><a class="readmore" href="blog.php" data-toggle="modal" data-target="#myModal2">Read More</a></div>-->
+				</div>
+			</li>
+			<li>
+				<div class="slider-info">
+					<h3>Welcome to Zaraa Farm Produce Marketers</h3>
+					<p>We provide for vegetables' marketing. Sell With Us</p>
+					<!--<div class="readmore-w3"><a class="readmore" href="blog.php" data-toggle="modal" data-target="#myModal2">Read More</a></div>-->
+				</div>
+			</li>
+		</ul>
+	<!--</div>-->
+	</nav>
+	<div class="clearfix"></div>
+	</div>
+       
+<div class="footer_bottom section" style="padding-left:23px; background-color:#fff; padding-top: 2px;">
+	<div class="agileits-w3layouts-footer">
+		<div class="container">
+			<div class="agileits_social_media team_agile team footer">
+			<ul class="social-media-icons">
+			<li><a href="www.facebook.com" class="facebook"> <i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+			<li><a href="www.twitter.com" class="twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+			</ul>
+	     </div>
+			
+			<div class="col-md-4 w3-agile-grid">
+				<h5>Address</h5>
+				<div class="w3-address">
+					<div class="w3-address-grid">
+						<div class="w3-address-left">
+							<i class="fa fa-phone" aria-hidden="true"></i>
+						</div>
+						<div class="w3-address-right">
+							<h6>Phone Number</h6>
+							<a href="tel:+254 726 044499"> Call Now</a>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="w3-address-grid">
+						<div class="w3-address-left">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</div>
+						<div class="w3-address-right">
+							<h6>Email Address</h6>
+							<p>Email :<a href="email:zaraafarmproducemarketers@gmail.com"> Send us an Email</a></p>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="w3-address-grid">
+						<div class="w3-address-left">
+							<i class="fa fa-map-marker" aria-hidden="true"></i>
+						</div>
+						<div class="w3-address-right">
+							<h6>Location</h6>
+							<p>Tom Mboya Street, Tudor, Mombasa, Kenya. 
+							</p>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+				</div>
+			</div>
+
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+<div class="copyright">
+	&copy;2018 Zaraa Farm Produce Marketers. All rights reserved
+</div>
+</div>
+
+
+
+<!--//footer-->
+
+<!-- js-files -->
+<!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<!-- //js -->
+<!-- gallery -->
+<script src="js/lightbox-plus-jquery.min.js"></script> 
+<!-- //gallery -->	
+<!-- banner-slider -->
+<script src="js/responsiveslides.min.js"></script>
+<script>
+// You can also use "$(window).load(function() {"
+$(function () {
+  // Slideshow 3
+  $("#slider3").responsiveSlides({
+	auto: true,
+	pager:true,
+	nav:true,
+	speed: 500,
+	namespace: "callbacks",
+	before: function () {
+	  $('.events').append("<li>before event fired.</li>");
+	},
+	after: function () {
+	  $('.events').append("<li>after event fired.</li>");
+	}
+  });
+
+});
+</script>
+<!-- //banner-slider -->
+<!-- start-smooth-scrolling-->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>	
+<script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event){		
+				event.preventDefault();
+		
+		$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+			});
+		});
+</script>
+<!-- //end-smooth-scrolling -->	
+<!-- smooth-scrolling-of-move-up -->
+<script type="text/javascript">
+	$(document).ready(function() {
+		/*
+		var defaults = {
+			containerID: 'toTop', // fading element id
+			containerHoverID: 'toTopHover', // fading element hover id
+			scrollSpeed: 1200,
+			easingType: 'linear' 
+		};
+		*/
+		
+		$().UItoTop({ easingType: 'easeOutQuart' });
+		
+	});
+</script>
+<!-- //smooth-scrolling-of-move-up -->
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/bootstrap.js"></script>
+<!-- //menu-js -->
+<script src="js/classie.js"></script>
+<script src="js/main.js"></script>
+<!-- //menu-js -->
+<!-- //js-files -->
+
+
+</body>
+</html>
